ab4c720ceda249084278625c0d63c8c8501a3095
a <102840087+letter-aa@users.noreply.github.com>
2025-04-20 14:21:15 -0400

    Beautify backend.js

--- backend.js
+++ backend.js
@@ -1,17 +1,17 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    switch (request.action) {
-        case "getCookie": {
+	switch (request.action) {
+		case "getCookie": {
 			chrome.cookies.get({
 				url: request.url,
 				name: request.name
 			}, sendResponse);
 
 			return true;
-        }
+		}
 
-        case "setCookie": {
-            chrome.cookies.set(request.arguments, sendResponse);
-			      return true;
-        }
-    }
+		case "setCookie": {
+			chrome.cookies.set(request.arguments, sendResponse);
+			return true;
+		}
+	}
 });
